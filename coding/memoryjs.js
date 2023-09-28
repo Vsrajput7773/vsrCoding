836d11e77a8cec9ec80fff715a4657b10da9479c
Vsrajput7773 <146314506+Vsrajput7773@users.noreply.github.com>
2023-09-28 05:58:31 +0000

    container config file done

---
+++ coding/memoryjs.js
@@ -0,0 +1,18 @@
+// javaScript has two types of memory 
+// 1.stack memory -: primitive data type - string ,number,bool. ,null,unfdefined,sybol,bigint.
+// 2.heap memory -: non primitive(reference) -array ,object ,function
+
+// stack memory
+
+let myYoutube ="Gita Gyan773";
+
+let anotherName =myYoutube;
+
+anotherName ="shree Krishna"
+
+console.log(anotherName);
+
+// it meins stack memory me kewal variable ki copy hi jati he orijanl value nhi jati he
+
+// heap memory 
+
